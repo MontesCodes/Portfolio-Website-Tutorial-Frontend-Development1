e77a8681e2887f66a29c029eb0565a917f06591e
MontesCodes-CodeGaming <MontiesWebDev@gmail.com>
2022-06-04 19:00:39 -0400

    HTML/CSS/SCSS updating 6/4/2022 #1

--- js/app.js
+++ js/app.js
@@ -16,5 +16,22 @@ function pageTransitions() {
       this.className += ' active-btn';
     });
   }
+  /// Sections Active class
+  allSections.addEventListener('click', e => {
+    const id = e.target.dataset.id;
+    if (id) {
+      /// Remove selected from the other button
+      sectBtns.forEach(btn => {
+        btn.classList.remove('active');
+      });
+      e.target.classList.add('active');
+      /// Hide other sections
+      sections.forEach(section => {
+        section.classList.remove('active');
+      });
+      const element = document.getElementById(id);
+      element.classList.add('active');
+    }
+  });
 }
 pageTransitions();
