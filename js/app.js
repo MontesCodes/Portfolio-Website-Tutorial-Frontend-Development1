9842e19b6eb1c86ec72b5d843ca0c89641d5f586
MontesCodes-CodeGaming <MontiesWebDev@gmail.com>
2022-06-04 17:01:46 -0400

    JS updating 6/4/2022 #1

--- js/app.js
+++ js/app.js
@@ -5,5 +5,16 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 
 function pageTransitions() {
-  /// Button click
+  /// Button click active class
+  for (let i = 0; i < sectBtn.length; i++) {
+    sectBtn[i].addEventListener('click', function () {
+      let currentBtn = document.querySelectorAll('.active-btn');
+      currentBtn[0].className = currentBtn[0].className.replace(
+        'active-btn',
+        '',
+      );
+      this.className += ' active-btn';
+    });
+  }
 }
+pageTransitions();
